f8378b42e14f542531c1a96f87f7b896091cc79d
Damon Oehlman <damon.oehlman@gmail.com>
2013-11-29 11:00:27 +1100

    general cleanup

--- test/getFeature.js
+++ test/getFeature.js
@@ -5,32 +5,32 @@ var wfs = require('../'),
     comparer = require('./helpers/comparer'),
     _ = require('underscore'),
     baseRequest = {
-        url: 'http://localhost:8080/geoserver/wfs',
-        typeName: 'gov:toilets'
+        url: 'http://envirohack.research.nicta.com.au/geotopo_250k/ows',
+        typeName: 'Terrain:caves'
     },
     testMin = new geojs.Pos('-28.94 138.01'),
     testMax = new geojs.Pos('-9.54 154.42'),
     testBounds = new geojs.BBox(testMin, testMax),
     rules = {
-        likeTown: {
+        isSinkhole: {
             type: 'like',
             args: {
-                property: 'name',
-                value: 'town*',
+                property: 'TEXTNOTE',
+                value: 'sinkhole',
                 matchCase: false
             }
         },
-        
+
         inQLD: {
             type: 'bbox',
             args: {
-                property: 'geom',
+                property: 'the_geom',
                 min: testMin.toString(),
                 max: testMax.toString()
             }
         }
     };
-    
+
 describe('getFeature tests', function() {
     it('should complain when "typeName" is not specified', function(done) {
         wfs.getXML('getFeature', function(err, data) {
@@ -38,10 +38,10 @@ describe('getFeature tests', function() {
             done();
         });
     });
-    
+
     it('should be able to generate the xml for a non-filtered request', function(done) {
         wfs.getXML('getFeature', baseRequest, function(err, data) {
-            comparer(data, 'getFeature-boston-sediment.xml', done);
+            comparer(data, 'getFeature-caves.xml', done);
         });
     });
 
@@ -49,62 +49,60 @@ describe('getFeature tests', function() {
         wfs.getFeature(baseRequest, function(err, results) {
             expect(results).to.be.ok();
             expect(results.type).to.equal('FeatureCollection');
-            
+
             // check that we have a features array
             expect(Array.isArray(results.features)).to.be.ok();
             expect(results.features.length).to.be.above(50);
-            
             // console.dir(results.features);
-            
             done(err);
         });
     });
-    
+
     it('should be able to make a filtered request to a test server', function(done) {
-        var ruleset = new geofilter.RuleSet([rules.likeTown]),
+        var ruleset = new geofilter.RuleSet([rules.isSinkhole]),
             filter = ruleset.to('ogc'),
             testRequest = _.extend({}, baseRequest, {
                 filter: filter
             });
-            
+
         wfs.getFeature(testRequest, function(err, results) {
             expect(results).to.be.ok();
             expect(results.type).to.equal('FeatureCollection');
-            
+
             // check that we have a features array
             expect(Array.isArray(results.features)).to.be.ok();
             expect(results.features.length).to.be.above(0);
-            
+
             results.features.forEach(function(feature) {
-                expect(feature.properties.name.toLowerCase().indexOf('town')).to.equal(0);
+                expect(feature.properties.TEXTNOTE.toLowerCase().indexOf('sinkhole')).to.equal(0);
             });
-            
+
             done(err);
         });
     });
-    
+
     it('should be able to make a filtered (two rules) request to a test server', function(done) {
-        var ruleset = new geofilter.RuleSet([rules.likeTown, rules.inQLD]),
+        var ruleset = new geofilter.RuleSet([rules.isSinkhole, rules.inQLD]),
             filter = ruleset.to('ogc'),
             testRequest = _.extend({}, baseRequest, {
                 filter: filter
             });
-            
+
         wfs.getFeature(testRequest, function(err, results) {
             expect(results).to.be.ok();
             expect(results.type).to.equal('FeatureCollection');
-            
+
             // check that we have a features array
             expect(Array.isArray(results.features)).to.be.ok();
             expect(results.features.length).to.be.above(0);
-            
+
             results.features.forEach(function(feature) {
                 var featurePos = new geojs.Pos(feature.geometry.coordinates[0], feature.geometry.coordinates[1]);
-                
-                expect(feature.properties.name.toLowerCase().indexOf('town')).to.equal(0);
+
+                expect(feature.properties.TEXTNOTE.toLowerCase().indexOf('sinkhole')).to.equal(0);
                 expect(testBounds.contains(featurePos)).to.be.ok();
             });
-            
+
             done(err);
         });
     });
--- test/helpers/comparer.js
+++ test/helpers/comparer.js
@@ -1,12 +1,12 @@
-var fs = require('fs'),
-    path = require('path'),
-    expect = require('expect.js'),
-    pathComparisons = path.resolve(__dirname, '..', 'comparisons');
+var fs = require('fs');
+var path = require('path');
+var expect = require('expect.js');
+var pathComparisons = path.resolve(__dirname, '..', 'comparisons');
 
 module.exports = function(data, targetFile, callback) {
-    fs.readFile(path.join(pathComparisons, targetFile), 'utf8', function(err, expectedData) {
-        expect(err).to.not.be.ok();
-        expect(data).to.equal(expectedData);
-        callback(err);
-    });
+  fs.readFile(path.join(pathComparisons, targetFile), 'utf8', function(err, expectedData) {
+    expect(err).to.not.be.ok();
+    expect(data).to.equal(expectedData);
+    callback(err);
+  });
 };
